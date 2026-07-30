5260df2c8d43d9457ecdfc3d88232d2810e53227
Chlna6666 <79357769+Chlna6666@users.noreply.github.com>
2026-07-30 14:55:20 +0800

    补充 GUID 常量构造

--- src/abi.rs
+++ src/abi.rs
@@ -18,6 +18,17 @@ pub struct Guid {
     pub data4: [u8; 8],
 }
 
+impl Guid {
+    pub const fn new(data1: u32, data2: u16, data3: u16, data4: [u8; 8]) -> Self {
+        Self {
+            data1,
+            data2,
+            data3,
+            data4,
+        }
+    }
+}
+
 pub type DllCanUnloadNowFn = unsafe extern "system" fn() -> HResult;
 pub type DllGetClassObjectFn =
     unsafe extern "system" fn(*const Guid, *const Guid, *mut *mut c_void) -> HResult;
